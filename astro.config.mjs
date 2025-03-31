82dbd535ce0b96917d46bb856169fd1f110e7e31
Yufa <112290188+01001101CK@users.noreply.github.com>
2025-03-31 10:59:03 -0700

    Added back sharp (#5)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://delta-docs-incubator.netlify.app/",
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+    },
+  },
   redirects: {
     "/": "/latest",
   },
