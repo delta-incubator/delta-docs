3ff8d66d0e3e42439b24b5d6c3afd2b11338e84f
Yufa <112290188+01001101CK@users.noreply.github.com>
2025-05-29 10:39:01 -0700

    Add a global banner and fix table formatting

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,16 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
-  docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({
+    type: "content",
+    schema: docsSchema({
+      extend: z.object({
+        banner: z.object({ content: z.string() }).default({
+          content:
+            'You\'re viewing the beta version. Looking for legacy docs? <a href="https://docs.delta.io" target="_blank" rel="noopener">Click here.</a>',
+        }),
+      }),
+    }),
+  }),
 };
