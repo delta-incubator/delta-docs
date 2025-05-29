dc225eaf8bbfde9235e83042701266640e08fabb
Jake Bellacera <hi@jakebellacera.com>
2025-05-29 03:47:37 +0000

    Port over updates from delta-io/delta fork
    - Updated pages and menu structure
    - Added redirects and netlify config files
    - Removed conflicting dependencies, such as husky and lint-staged
    - Updated favicon and other assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,177 +10,143 @@ export default defineConfig({
       entrypoint: "astro/assets/services/sharp",
     },
   },
-  redirects: {
-    "/": "/latest",
-  },
   integrations: [
     starlight({
       customCss: ["./src/styles/custom.css"],
       title: "Delta Lake",
-      social: {
-        github: "https://github.com/delta-io/delta",
-      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/delta-io/delta",
+        },
+      ],
       editLink: {
         baseUrl:
           "https://github.com/jakebellacera/db-site-staging/tree/main/sites/delta-docs",
       },
       lastUpdated: true,
+      favicon: "/favicon.svg",
       logo: {
         light: "./src/assets/delta-lake-logo-light.svg",
         dark: "./src/assets/delta-lake-logo-dark.svg",
         replacesTitle: true,
       },
       sidebar: [
-        { label: "Welcome", link: "/latest/" },
+        { label: "Introduction", link: "/" },
         {
           label: "Apache Spark connector",
+          collapsed: true,
           items: [
             {
-              label: "Quickstart",
-              link: "/latest/apache-spark-connector/quickstart/",
+              slug: "quick-start",
             },
             {
-              label: "Table batch reads and writes",
-              link: "/latest/apache-spark-connector/table-batch-reads-and-writes/",
+              slug: "delta-batch",
             },
             {
-              label: "Table streaming reads and writes",
-              link: "/latest/apache-spark-connector/table-streaming-reads-and-writes/",
+              slug: "delta-streaming",
             },
             {
-              label: "Table deletes, updates, and merges",
-              link: "/latest/apache-spark-connector/table-deletes-updates-and-merges/",
+              slug: "delta-update",
             },
             {
-              label: "Change data feed",
-              link: "/latest/apache-spark-connector/change-data-feed/",
+              slug: "delta-change-data-feed",
             },
             {
-              label: "Table utility commands",
-              link: "/latest/apache-spark-connector/table-utility-commands/",
+              slug: "delta-utility",
             },
             {
-              label: "Constraints",
-              link: "/latest/apache-spark-connector/constraints/",
+              slug: "delta-constraints",
             },
             {
-              label: "How does Delta Lake manage feature compatibility?",
-              link: "/latest/apache-spark-connector/how-does-delta-lake-manage-feature-compatibility/",
+              slug: "versioning",
             },
             {
-              label: "Delta default column values",
-              link: "/latest/apache-spark-connector/delta-default-column-values/",
+              slug: "delta-default-columns",
             },
             {
-              label: "Delta column mapping",
-              link: "/latest/apache-spark-connector/delta-column-mapping/",
+              slug: "delta-column-mapping",
             },
             {
-              label: "Use liquid clustering for Delta tables",
-              link: "/latest/apache-spark-connector/use-liquid-clustering-for-delta-tables/",
+              slug: "delta-clustering",
             },
             {
-              label: "What are deletion vectors?",
-              link: "/latest/apache-spark-connector/what-are-deletion-vectors/",
+              slug: "delta-deletion-vectors",
             },
             {
-              label: "Drop Delta table features",
-              link: "/latest/apache-spark-connector/drop-delta-table-features/",
+              slug: "delta-drop-feature",
             },
             {
-              label: "Use row tracking for Delta tables",
-              link: "/latest/apache-spark-connector/use-row-tracking-for-delta-tables/",
+              slug: "delta-row-tracking",
             },
             {
-              label: "Storage configuration",
-              link: "/latest/apache-spark-connector/storage-configuration/",
+              slug: "delta-storage",
             },
             {
-              label: "Delta type widening",
-              link: "/latest/apache-spark-connector/delta-type-widening/",
+              slug: "delta-type-widening",
             },
             {
-              label: "Universal Format (UniForm)",
-              link: "/latest/apache-spark-connector/universal-format-uniform/",
+              slug: "delta-uniform",
             },
             {
-              label: "Read Delta Sharing Tables",
-              link: "/latest/apache-spark-connector/read-delta-sharing-tables/",
+              slug: "delta-sharing",
             },
             {
-              label: "Concurrency control",
-              link: "/latest/apache-spark-connector/concurrency-control/",
+              slug: "concurrency-control",
             },
             {
-              label: "Migration guide",
-              link: "/latest/apache-spark-connector/migration-guide/",
+              slug: "porting",
             },
             {
-              label: "Best practices",
-              link: "/latest/apache-spark-connector/best-practices/",
+              slug: "best-practices",
             },
             {
-              label: "Frequently asked questions (FAQ)",
-              link: "/latest/apache-spark-connector/faq/",
+              slug: "delta-faq",
             },
             {
-              label: "Optimizations",
-              link: "/latest/apache-spark-connector/optimizations/",
+              slug: "optimizations-oss",
             },
           ],
         },
         {
-          label: "Trino connector",
-          link: "/latest/trino-connector/",
+          slug: "delta-trino-integration",
         },
         {
-          label: "Presto connector",
-          link: "/latest/presto-connector/",
+          slug: "delta-presto-integration",
         },
         {
-          label:
-            "Presto, Trino, and Athena to Delta Lake integration using manifests",
-          link: "/latest/presto-integration/",
+          slug: "redshift-spectrum-integration",
         },
         {
-          label: "AWS Redshift Spectrum connector",
-          link: "/latest/aws-redshift-spectrum-connector/",
+          slug: "snowflake-integration",
         },
         {
-          label: "Snowflake integration",
-          link: "/latest/snowflake-integration/",
+          slug: "bigquery-integration",
         },
         {
-          label: "Google BigQuery connector",
-          link: "/latest/bigquery-integration/",
+          slug: "flink-integration",
         },
         {
-          label: "Apache Flink connector",
-          link: "/latest/flink-integration/",
+          slug: "delta-more-connectors",
         },
         {
-          label: "Delta more connectors",
-          link: "/latest/delta-more-connectors/",
+          slug: "delta-kernel",
         },
         {
-          label: "Delta Kernel",
-          link: "/latest/delta-kernel/",
+          slug: "delta-standalone",
         },
         {
-          label: "Delta Lake APIs",
-          link: "/latest/delta-lake-apis/",
+          slug: "delta-apidoc",
         },
         {
-          label: "Releases",
-          link: "/latest/releases/",
+          slug: "releases",
         },
         {
-          label: "Delta Lake Resources",
-          link: "/latest/delta-resources/",
+          slug: "delta-resources",
         },
         {
-          label: "Delta table properties reference",
-          link: "/latest/table-properties/",
+          slug: "table-properties",
         },
         {
           label: "Contribute",
